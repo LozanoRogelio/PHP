bb0899858ede33855443b207f836f2b265c3b372
Rogelio Lozano <lozano.rogelio92@gmail.com>
2023-04-13 15:44:10 -0500

    Created products class and added properties

--- OOP/creating-classes.php
+++ OOP/creating-classes.php
@@ -1,4 +1,23 @@
 <?php
 
+class Product
+{
+   public $name = 'soap';
+   public $price = 5;
+}
 
+
+
+$soapObject = new Product();
+print $soapObject->name . "<br>";
+print $soapObject->price . "<br>";
+$soapObject->name = 'toothpaste';
+$soapObject->price = 2;
+print $soapObject->name . "<br>";
+print $soapObject->price . "<br>";
+// Creating class
+// $product = new Product();
+// $anotherProduct = new Product();
+// var_dump($product);
+// var_dump($anotherProduct);
 ?>
